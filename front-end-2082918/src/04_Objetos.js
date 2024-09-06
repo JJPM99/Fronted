853eecfb0e576523b7b28e9500361f22d81bafce
José Javier Puerta Mariñez <64931824+JJPM99@users.noreply.github.com>
2024-09-05 20:15:27 -0600

    Sexto commit
    Ahora vamos a ver objetos relacionados, en cual vamos a tomar como referencia el commit anterior, solo que ahora vamos a escribir otros atributos como objeto dentro de este mismo.

---
+++ front-end-2082918/src/04_Objetos.js
@@ -0,0 +1,13 @@
+//objetos en javascript
+const game = {
+
+    id: 1,
+    name: 'god of war ',
+    dev: 'Santa Monica Studios',
+    console: 'playstation consoles',
+    cost: 800,
+    yearRelease: 2018
+
+}
+
+console.log(game)
--- front-end-2082918/src/main.js
+++ front-end-2082918/src/main.js
@@ -1,13 +1,22 @@
-//objetos en javascript
+
+// objetos relacionados y funciones 
 const game = {
 
     id: 1,
     name: 'god of war ',
     dev: 'Santa Monica Studios',
     console: 'playstation consoles',
+    brand: {
+        id: 1,
+        name: 'Sony',
+        location: 'USA',
+    },
     cost: 800,
-    yearRelease: 2018
+    yearRelease: 2018,
+    status: 'available',
+    stock: function () {
+        return `this product is currently: ${this.status}`
+    } 
 
 }
 
-console.log(game)
