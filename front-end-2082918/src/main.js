5d0ef6a3f638e4c816a4d51deb7b6ae6dbe113c7
José Javier Puerta Mariñez <64931824+JJPM99@users.noreply.github.com>
2024-09-05 19:41:14 -0600

    Segundo commit
    Empezamos la introduccion a Javascript con el tema de Strings y Variables.

---
+++ front-end-2082918/src/main.js
@@ -0,0 +1,16 @@
+// variables y constantes 
+
+name = 'Jose';
+let lastname = 'Puerta';
+
+const age = 25;
+
+console.log('Hola! bienvenido ' + name + ' '+ lastname + ', tu edad es: '+age);
+
+// template string 
+
+let degree = 'Ciencias Computacionales '
+let faculty = 'Fisico-Matematicas'
+
+console.log(`Eres de la carrera de ${degree} y de la facultad de ${faculty}`);
+
