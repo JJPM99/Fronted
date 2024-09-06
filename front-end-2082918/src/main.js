4985c94ab63833ca155e3d77ecfcf536d7d5bf5b
José Javier Puerta Mariñez <64931824+JJPM99@users.noreply.github.com>
2024-09-05 20:23:09 -0600

    Septimo commit
    Ahora vamos a hacer un arreglo de objetos, donde ahora vamos a utilizar un conjunto de objetos con parametros similares a los ejemplos anteriores pero ahora vamos a estructurarlo utilizando un arreglo para colocar varios objetos.

--- front-end-2082918/src/main.js
+++ front-end-2082918/src/main.js
@@ -1,22 +1,52 @@
+    // arreglos en objetos 
 
-// objetos relacionados y funciones 
-const game = {
+    const game = {
 
-    id: 1,
-    name: 'god of war ',
-    dev: 'Santa Monica Studios',
-    console: 'playstation consoles',
-    brand: {
         id: 1,
-        name: 'Sony',
-        location: 'USA',
-    },
-    cost: 800,
-    yearRelease: 2018,
-    status: 'available',
-    stock: function () {
-        return `this product is currently: ${this.status}`
-    } 
+        name: 'god of war ',
+        dev: 'Santa Monica Studios',
+        console: 'playstation consoles',
+        brand: {
+            id: 1,
+            name: 'Sony',
+            location: 'USA',
+        },
+        dificult: [
 
-}
+        { id:1,
+        level: 'very easy',
+        description: 'not a challenge',
+        hoursToComplete: 20
+        },
+        { id:2,
+            level: 'medium',
+            description: 'enemies are stronger and you are limited',
+            hoursToComplete: 23       },
+        { id:3,
+            level: 'hard',
+            description: 'enemies will defeat you with one attack',
+            hoursToComplete: 28
+        },
 
+        ]    
+        ,
+        timeComplete: function () {
+            let total = 0;
+            this.dificult.forEach( difficulties => {
+                total = total + difficulties.hoursToComplete;
+            })
+            return total;   
+        }
+        ,
+        cost: 800,
+        yearRelease: 2018,
+        status: 'available',
+        stock: function () {
+            return `this game is currently: ${this.status}`
+        } 
+
+    }
+
+
+    console.log(game.timeComplete());
+    console.log(game);
